21dd78d52d18cc46d84f617e37d375389e26aaa5
Daniel Hoang <thesubway@gmail.com>
2016-05-26 16:29:07 -0700

    Add songs app files

---
+++ main.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'slim'
+require 'sass'
+require './song'
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  slim :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do
+  slim :contact
+end
+
+not_found do
+  slim :not_found
+end
+
